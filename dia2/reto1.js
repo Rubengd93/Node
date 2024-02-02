4945494a8115a1c510f103de95558be2ea9a86f0
Rubengd93 <rubengd93@gmail.com>
2024-02-02 19:27:35 +0100

    Modificacion reto 1

--- dia2/reto1.js
+++ dia2/reto1.js
@@ -1,23 +1,13 @@
-const readline = require("readline");
 const fs = require("fs/promises");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 async function createPerson() {
+  const obj = {
+    name: "John",
+    surname: "Doe",
+    age: 30
+  };
+
   try {
-    const name = await questionAsync("Introduce tu nombre: ");
-    const surname = await questionAsync("Introduce tu apellido: ");
-    const age = await questionAsync("Introduce tu edad: ");
-
-    const obj = {
-      name: name,
-      surname: surname,
-      age: age
-    };
-
     await fs.writeFile("person.json", JSON.stringify(obj));
     console.log("El archivo ha sido creado exitosamente.");
 
@@ -29,13 +19,7 @@ async function createPerson() {
     console.log("El archivo ha sido borrado exitosamente.");
   } catch (err) {
     console.error(err);
-  } 
-}
-
-function questionAsync(prompt) {
-  return new Promise((resolve) => {
-    rl.question(prompt, resolve);
-  });
+  }
 }
 
 createPerson();
